a63abc4cd19c1cf3f11bcb8e883dae5ba0c2453b
Himanshu <himanshu@localhost.localdomain>
2020-09-24 21:10:20 +0530

    Passport used for authentication

--- app.js
+++ app.js
@@ -3,26 +3,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
 const app=express();
-
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true,useUnifiedTopology: true});
+app.use(session({
+    secret:"Our little secret.",
+    resave: false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema ({
     email:String,
     password:String
 });
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields:["password"]});
+
+userSchema.plugin(passportLocalMongoose);//used to hash and salt the password
 
 const User=new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());//to create a local mongo stratiges 
 
-
+passport.serializeUser(User.serializeUser());//creating cookie
+passport.deserializeUser(User.deserializeUser());//exposing the content of cookie
 
 app.get("/",function(req,res){
 
@@ -32,44 +43,59 @@ app.get("/login",function(req,res){
     
     res.render("login");
 });
-app.post("/login",function(req,res){
-    const username=req.body.username;
-    const password=req.body.password;
 
-    User.findOne({email:username},function(err,foundUser){
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
-            }
-        }
-    });
-});
+
 app.get("/register",function(req,res){
-    
     res.render("register");
 });
-
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
 app.post("/register",function(req,res){
-    const newUser= new User({
-         email:req.body.username,
-         password:req.body.password
+
+     User.register({username:req.body.username},req.body.password,function(err,user){
+         if(err){
+            console.log(err);
+            res.redirect("/register");
+         }
+         else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+         }
     });
-    newUser.save(function(err){
+    
+    
+});
+app.post("/login",function(req,res){
+    const user= new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+    req.login(user,function(err){ //we utilise the passport's function login() which automatically check for the password as the user provided.
         if(err){
             console.log(err);
         }
         else{
-            res.render("secrets");
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
     });
-});
 
+
+
+});
 
 
 
